a60fbdd13bcbde55534013fef55fb4c37e540c8b
Yevhen Holienishchev <Yevhen Holienishchev>
2016-07-09 21:22:06 +0300

    Added: modifiers, parameters, annotations

--- Main.java
+++ Main.java
@@ -1,23 +1,43 @@
 package com.example.lesson27;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class Main {
     public static void main(String[] args) {
         Class c = Thread.class;
-        System.out.println(c.getName());
         int modifiers = c.getModifiers();
+        System.out.print(c.getName() + " Modifiers:");
         if (Modifier.isPrivate(modifiers)) {
-            System.out.println("private");
+            System.out.println(" private");
         }
         if (Modifier.isPublic(modifiers)) {
-            System.out.println("public");
+            System.out.print(" public");
         }
         if (Modifier.isFinal(modifiers)) {
-            System.out.println("final");
+            System.out.println(" final");
         }
         if (Modifier.isStatic(modifiers)) {
-            System.out.println("static");
+            System.out.println(" static");
+        }
+        System.out.println("\n");
+        Method[] methods = c.getMethods();
+        for (Method method : methods){
+            System.out.println("Method Name: " + method.getName());
+            System.out.println("Return Type: " + method.getReturnType().getName());
+            Class[] paramTypes = method.getParameterTypes();
+            System.out.print("Parameter Type: ");
+            for (Class paramType : paramTypes) {
+                System.out.print(paramType.getName());
+            }
+            System.out.println();
+            Annotation[] annotations = method.getAnnotations();
+            System.out.print("Annotation: ");
+            for (Annotation annotation : annotations) {
+                System.out.print("@" + annotation.annotationType().getSimpleName());
+            }
+            System.out.println("\n");
         }
     }
 }
